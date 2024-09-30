a2ee4e2e5c80991b054a3c514f7e0227c56ea205
Derek W. S. Gray <dgray@uncharted.software>
2024-09-30 11:04:38 -0400

    simplify If/Else/etc component import

---
+++ packages/jsonforms-primevue/src/components/blocks/index.ts
@@ -0,0 +1,4 @@
+export { default as If } from "./If.vue";
+export { default as ElseIf } from "./ElseIf.vue";
+export { default as Else } from "./Else.vue";
+export { default as For } from "./For.vue";
